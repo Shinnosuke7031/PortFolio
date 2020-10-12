c6a5cc4b98ec4f07aad4044cb888c14154a63a46
Shinnosuke Iino <sengoku731sin@gmail.com>
2020-10-13 01:58:08 +0900

    Update skills used

--- src/views/pages/AboutThisSite.tsx
+++ src/views/pages/AboutThisSite.tsx
@@ -3,7 +3,7 @@ import SiteDetailInfoes from '../templates/PC_Components/SiteDetailInfoes';
 import SiteDetailInfoesMob from '../templates/MobileComponents/SiteDetailInfoesMob';
 import MediaQuery from "react-responsive";
 
-const skills = ['React.js', 'TypeScript', 'CSS Module', 'React Router', 'Material UI'];
+const skills = ['React.js', 'TypeScript', 'Gatsby.js', 'Context API', 'CSS Module', 'Material UI'];
 
 const AboutThisSite: FC<{}> = () => {
 
